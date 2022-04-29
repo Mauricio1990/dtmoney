6e453f6dc5b4c931741cb16a83262ad32358db77
Mauricio Nascimento de Oliveira <mauricio.nascimento@havan.com.br>
2022-04-29 14:21:23 -0300

    Cria tabela de lançamentos de saques e depósitos

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './style';
+
+interface HeaderPropos {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderPropos){
+    
+
+    return (
+       <Container>
+           <Content>
+            <img src={logoImg} alt="dt money" />
+            <button type="button" onClick={onOpenNewTransactionModal}>
+                Nova Transação
+            </button>           
+           </Content>
+       </Container>
+    )
+}
